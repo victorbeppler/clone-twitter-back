bdbd112fac21ec0bf3748e1c7e477630bb21e8ee
victorbeppler <victorhugobeppler@gmail.com>
2022-05-30 01:04:32 -0300

    Adicionando o .env no ignore

--- src/rotas/rota-criar-comentario.js
+++ src/rotas/rota-criar-comentario.js
@@ -7,6 +7,7 @@ const routeName = "createComment";
 router.post(`/${routeName}`, async (req, res) => {
     const request = req.body;
     const comentarioCriado = await new CriarComentario().executar(request);
+    
     res.status(201).send("Comentário Criado");
 });
 
